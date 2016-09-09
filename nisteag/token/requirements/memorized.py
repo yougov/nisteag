c6e3d16f95f1a95a979a99821d384a9407dbe1a2
Diogo Baeder de Paula Pinto <diogobaeder@yahoo.com.br>
2016-09-08 22:23:41 -0300

    Level 2 checker

--- nisteag/token/requirements/memorized.py
+++ nisteag/token/requirements/memorized.py
@@ -6,10 +6,20 @@ class SmallPasswordError(StrengthError):
     """Raised when the password is too small."""
 
 
-class Level1Checker(object):
-    MINIMUM_LENGTH = 6
-
-    def check(self, password):
+class BaseMemorizedChecker(object):
+    def _check_length(self, password):
         if len(password) < self.MINIMUM_LENGTH:
             raise SmallPasswordError(
-                'The password needs to be at least 8 characters long.')
+                'The password needs to be at least {} characters long.'.format(
+                    self.MINIMUM_LENGTH))
+
+    def check(self, password):
+        self._check_length(password)
+
+
+class Level1Checker(BaseMemorizedChecker):
+    MINIMUM_LENGTH = 6
+
+
+class Level2Checker(BaseMemorizedChecker):
+    MINIMUM_LENGTH = 8
--- tests/token/requirements/test_memorized.py
+++ tests/token/requirements/test_memorized.py
@@ -4,6 +4,7 @@ from nose.tools import istest
 
 from nisteag.token.requirements.memorized import (
     Level1Checker,
+    Level2Checker,
     SmallPasswordError,
 )
 
@@ -20,3 +21,17 @@ class Level1CheckerTest(TestCase):
     def fails_a_too_small_password(self):
         with self.assertRaises(SmallPasswordError):
             self.checker.check('12345')
+
+
+class Level2CheckerTest(TestCase):
+    def setUp(self):
+        self.checker = Level2Checker()
+
+    @istest
+    def checks_a_strong_password(self):
+        self.checker.check('This Is a BIG and relev4nt passwurd!!!')
+
+    @istest
+    def fails_a_too_small_password(self):
+        with self.assertRaises(SmallPasswordError):
+            self.checker.check('1234567')
